da64bbe1c3d709b241eabae5f5c7463f499f52d4
zhangwenhai <zhangwenhai6@163.com>
2016-06-16 16:36:18 +0800

    添加全部评论界面

--- scripts/example.js
+++ scripts/example.js
@@ -27,7 +27,7 @@ var Comment = React.createClass({
         if (this.props.date.getTime() - time < 60 * 1000) {
             return parseInt((this.props.date.getTime() - time) / 1000) + "秒之前";
         }
-        if (this.props.date.getTime() - time < 15 * 60 * 1000) {
+        if (this.props.date.getTime() - time < 60 * 60 * 1000) {
             return parseInt((this.props.date.getTime() - time) / 1000 / 60) + "分钟之前";
         }
         if (this.props.date.getTime() - time < 24 * 60 * 60 * 1000) {
@@ -88,31 +88,49 @@ var CommentBox = React.createClass({
 
     getUrlParam: function () {
         this.url = "/v3/topic/detail?id=" + window.topic_id;
+        this.CommentSubmiturl = "/v3/topic/comment?topicId=" + window.topic_id;
         console.log(this.url);
     },
 
     getUrlParam1: function () {
-        location.href = 'http://www.baidu.com';
+        location.href = '../comment.html?id=' + window.topic_id;
     },
 
     handleCommentSubmit: function (comment) {
-        var comments = this.state.data;
-        comment.id = Date.now();
-        var newComments = comments.concat([comment]);
-        this.setState({data: newComments});
-        $.ajax({
-            url: this.props.url,
-            dataType: 'json',
-            type: 'POST',
-            data: comment,
-            success: function (data) {
-                this.setState({data: data});
-            }.bind(this),
-            error: function (xhr, status, err) {
-                this.setState({data: comments});
-                console.error(this.props.url, status, err.toString());
-            }.bind(this)
-        });
+        console.log(comment.text);
+        console.log(document.cookie);
+        document.cookie = "auth-uid=1";
+        document.cookie = "auth-token=52e95cfe45ac48388b59124cf823d288";
+        var strCookie = document.cookie;
+        var arrCookie = strCookie.split("; ");
+        var auth_uid;
+        var auth_token;
+        for (var i = 0; i < arrCookie.length; i++) {
+            var arr = arrCookie[i].split("=");
+            if ("auth-uid" == arr[0]) {
+                auth_uid = arr[1];
+            } else if ("auth-token" == arr[0]) {
+                auth_token = arr[1];
+            }
+        }
+        console.log(auth_uid);
+        console.log(auth_token);
+        fetch(this.CommentSubmiturl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                "auth-uid": auth_uid,
+                "auth-token": auth_token,
+            },
+            body: "content=" + comment.text,
+        })
+            .then((response) => response.json())
+            .then((responseData) => {
+                console.log(responseData);
+            })
+            .catch(error => {
+            });
+
     },
 
     getInitialState: function () {
@@ -133,9 +151,128 @@ var CommentBox = React.createClass({
                 <div className="commentLine"/>
 
                 <div className="commentHead_1"></div>
-                <CommentList data={this.state.data}/>
+                <CommentList data={this.state.data.comments}/>
 
                 <span className="commentFoot" onClick={this.getUrlParam1}>查看全部评论</span>
+
+                <div style={{width:"100%",height:54}}></div>
+                <div className="commentLine" style={{position:"fixed",bottom:55,border:0}}></div>
+                <CommentForm onCommentSubmit={this.handleCommentSubmit}/>
+            </div>
+        );
+    }
+});
+
+var CommentBox2 = React.createClass({
+    getInitialState: function () {
+        return {data: []};
+    },
+
+    componentDidMount: function () {
+        this.pageNum = 1;
+        this.isEnd = false;
+        this.loadCommentsFromServer();
+        window.onscroll = function () {
+            if (getScrollTop() + getClientHeight() == getScrollHeight()) {
+                this.loadCommentsFromServer();
+            }
+        }.bind(this);
+    },
+
+    getUrlParam: function (pageNum) {
+        var urlParams;
+        var match,
+            pl = /\+/g,  // Regex for replacing addition symbol with a space
+            search = /([^&=]+)=?([^&]*)/g,
+            decode = function (s) {
+                return decodeURIComponent(s.replace(pl, " "));
+            },
+            query = window.location.search.substring(1);
+        urlParams = {};
+        while (match = search.exec(query))
+            urlParams[decode(match[1])] = decode(match[2]);
+        this.url = "/v3/topic/comments?topicId=" + urlParams["id"] + "&pageNum=" + pageNum + "&pageSize=20";
+        this.CommentSubmiturl = "/v3/topic/comment?topicId=" + urlParams["id"];
+        console.log(this.url);
+    },
+
+    loadCommentsFromServer: function () {
+        if (this.isEnd) {
+            return;
+        }
+        this.getUrlParam(this.pageNum);
+        fetch(this.url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+        })
+            .then((response) => response.json())
+            .then((responseData) => {
+                console.log(responseData);
+                if (responseData.data.length < 20) {
+                    this.isEnd = true;
+                } else {
+                    this.isEnd = false;
+                }
+                var data = this.state.data;
+                if (this.pageNum == 1) {
+                    data = responseData.data;
+                } else {
+                    data = data.concat(responseData.data);
+                }
+                this.setState({data: data});
+                this.pageNum = this.pageNum + 1;
+            })
+            .catch(error => {
+            });
+    },
+
+    getUrlParam1: function () {
+        location.href = '../comment.html?id=' + window.topic_id;
+    },
+
+    handleCommentSubmit: function (comment) {
+        console.log(comment.text);
+        console.log(document.cookie);
+        document.cookie = "auth-uid=1";
+        document.cookie = "auth-token=52e95cfe45ac48388b59124cf823d288";
+        var strCookie = document.cookie;
+        var arrCookie = strCookie.split("; ");
+        var auth_uid;
+        var auth_token;
+        for (var i = 0; i < arrCookie.length; i++) {
+            var arr = arrCookie[i].split("=");
+            if ("auth-uid" == arr[0]) {
+                auth_uid = arr[1];
+            } else if ("auth-token" == arr[0]) {
+                auth_token = arr[1];
+            }
+        }
+        console.log(auth_uid);
+        console.log(auth_token);
+        fetch(this.CommentSubmiturl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                "auth-uid": auth_uid,
+                "auth-token": auth_token,
+            },
+            body: "content=" + comment.text,
+        })
+            .then((response) => response.json())
+            .then((responseData) => {
+                alert(responseData.data);
+                this.loadCommentsFromServer();
+            })
+            .catch(error => {
+            });
+    },
+
+    render: function () {
+        return (
+            <div className="commentBox">
+                <CommentList data={this.state.data}/>
 
                 <div style={{width:"100%",height:54}}></div>
                 <div className="commentLine" style={{position:"fixed",bottom:55,border:0}}></div>
@@ -148,10 +285,10 @@ var CommentBox = React.createClass({
 var CommentList = React.createClass({
     render: function () {
         var data = new Date();
-        if (!this.props.data.comments) {
+        if (!this.props.data) {
             return null;
         }
-        var commentNodes = this.props.data.comments.map(function (comment, index) {
+        var commentNodes = this.props.data.map(function (comment) {
             return (
                 <Comment key={comment.id} comment={comment} date={data}/>
             );
@@ -197,9 +334,45 @@ var CommentForm = React.createClass({
 });
 
 window.onload = function () {
-    ReactDOM.render(
-        <CommentBox />,
-        document.getElementById('detail_comment')
-    );
-}
+    if (document.getElementById('detail_comment')) {
+        ReactDOM.render(
+            <CommentBox />,
+            document.getElementById('detail_comment')
+        );
+    } else if (document.getElementById('comment_list')) {
+        ReactDOM.render(
+            <CommentBox2 />,
+            document.getElementById('comment_list')
+        );
+    }
+};
+
+function getScrollTop() {
+    var scrollTop = 0;
+    if (document.documentElement && document.documentElement.scrollTop) {
+        scrollTop = document.documentElement.scrollTop;
+    }
+    else if (document.body) {
+        scrollTop = document.body.scrollTop;
+    }
+    return scrollTop;
+};
+
+//获取当前可是范围的高度
+function getClientHeight() {
+    var clientHeight = 0;
+    if (document.body.clientHeight && document.documentElement.clientHeight) {
+        clientHeight = Math.min(document.body.clientHeight, document.documentElement.clientHeight);
+    }
+    else {
+        clientHeight = Math.max(document.body.clientHeight, document.documentElement.clientHeight);
+    }
+    return clientHeight;
+};
+
+//获取文档完整的高度
+function getScrollHeight() {
+    return Math.max(document.body.scrollHeight, document.documentElement.scrollHeight);
+};
+
 
